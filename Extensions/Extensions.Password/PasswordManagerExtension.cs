6b2bb88ec6ba33a2252b0e26b535955212ce0594
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-19 17:25:59 +0300

    Added password extension project

---
+++ Extensions/Extensions.Password/PasswordManagerExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+namespace Extensions.Password
+{
+    public static class PasswordManagerExtension
+    {
+        public static bool VerifyPasswordHash(this string password, string passwordHash)
+        {
+            return PasswordManager.VerifyPasswordHash(password, passwordHash);
+        }
+
+        public static string GeneratePasswordHash(this string password, KeyDerivationPrf prf = KeyDerivationPrf.HMACSHA256, int iterationCount = 10000, int saltSize = 16)
+        {
+            return PasswordManager.GeneratePasswordHash(password, prf, iterationCount, saltSize);
+        }
+    }
+}
