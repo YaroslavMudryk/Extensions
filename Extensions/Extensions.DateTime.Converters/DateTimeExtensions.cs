339924848bf087c832d86a258e402745265d821d
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-19 17:05:48 +0300

    Added basic convertors from\to unix time

---
+++ Extensions/Extensions.DateTime.Converters/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Extensions.DateTime.Converters
+{
+    public static class DateTimeExtensions
+    {
+        public static long ToUnix(this System.DateTime dateTime)
+        {
+            var dTime = new System.DateTime(1970, 0, 0, 0, 0, 0, DateTimeKind.Utc);
+            return (long)(dateTime - dTime).TotalSeconds;
+        }
+
+        public static System.DateTime ToDateTime(long unixTime)
+        {
+            var dTime = new System.DateTime(1970, 0, 0, 0, 0, 0, DateTimeKind.Utc);
+            return dTime.AddSeconds(unixTime);
+        }
+    }
+}
