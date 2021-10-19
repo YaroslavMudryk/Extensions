13ca65f9a42e1e5d44e984836160826a93975319
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-19 17:50:09 +0300

    Added project for http extensions

---
+++ Extensions/Extensions.HttpClient/HttpExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http.Headers;
+
+namespace Extensions.HttpClient
+{
+    public static class HttpExtensions
+    {
+        public static void SetBearerToken(this System.Net.Http.HttpClient httpClient, string token)
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+}
