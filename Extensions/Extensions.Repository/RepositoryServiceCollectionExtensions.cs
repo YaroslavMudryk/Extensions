02a3e56c7c24a0654a6c33b1899c5e800eabbfa8
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-20 00:06:54 +0300

    [Extension.Repository] Added the ability to run multiple contexts simultaneously (up to 4)

--- Extensions/Extensions.Repository/RepositoryServiceCollectionExtensions.cs
+++ Extensions/Extensions.Repository/RepositoryServiceCollectionExtensions.cs
@@ -4,9 +4,46 @@ namespace Extensions.Repository
 {
     public static class RepositoryServiceCollectionExtensions
     {
-        public static void AddRepositories<TContext>(this IServiceCollection services) where TContext: DbContext
+        public static void AddRepositories<TContext>(this IServiceCollection services)
+            where TContext : DbContext
         {
             services.AddScoped<DbContext, TContext>();
+            services.AddScoped(typeof(IAsyncReadRepository<>), typeof(AsyncReadRepository<>));
+            services.AddScoped(typeof(IAsyncWriteRepository<>), typeof(AsyncWriteRepository<>));
+        }
+
+        public static void AddRepositories<TContext1, TContext2>(this IServiceCollection services)
+            where TContext1 : DbContext
+            where TContext2 : DbContext
+        {
+            services.AddScoped<DbContext, TContext1>();
+            services.AddScoped<DbContext, TContext2>();
+            services.AddScoped(typeof(IAsyncReadRepository<>), typeof(AsyncReadRepository<>));
+            services.AddScoped(typeof(IAsyncWriteRepository<>), typeof(AsyncWriteRepository<>));
+        }
+
+        public static void AddRepositories<TContext1, TContext2, TContext3>(this IServiceCollection services)
+            where TContext1 : DbContext
+            where TContext2 : DbContext
+            where TContext3 : DbContext
+        {
+            services.AddScoped<DbContext, TContext1>();
+            services.AddScoped<DbContext, TContext2>();
+            services.AddScoped<DbContext, TContext3>();
+            services.AddScoped(typeof(IAsyncReadRepository<>), typeof(AsyncReadRepository<>));
+            services.AddScoped(typeof(IAsyncWriteRepository<>), typeof(AsyncWriteRepository<>));
+        }
+
+        public static void AddRepositories<TContext1, TContext2, TContext3, TContext4>(this IServiceCollection services)
+            where TContext1 : DbContext
+            where TContext2 : DbContext
+            where TContext3 : DbContext
+            where TContext4 : DbContext
+        {
+            services.AddScoped<DbContext, TContext1>();
+            services.AddScoped<DbContext, TContext2>();
+            services.AddScoped<DbContext, TContext3>();
+            services.AddScoped<DbContext, TContext4>();
             services.AddScoped(typeof(IAsyncReadRepository<>), typeof(AsyncReadRepository<>));
             services.AddScoped(typeof(IAsyncWriteRepository<>), typeof(AsyncWriteRepository<>));
         }
