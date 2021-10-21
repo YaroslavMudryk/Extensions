3c0d17f74ec4c747dc26a2eb4688664ad39071ac
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-21 23:41:22 +0300

    [Extensions.IP] Added new project

---
+++ Extensions/Extensions.IP/IpClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+namespace Extensions.IP
+{
+    public class IpClient
+    {
+        public static async Task<IPInfo> GetIpInfoAsync(string ipAddress)
+        {
+            string url = null;
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                url = "json?fields=66846719";
+            }
+            else
+            {
+                url = $"json/{ipAddress}?fields=66846719";
+            }
+            HttpClient httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://ip-api.com"),
+                Timeout = TimeSpan.FromSeconds(20)
+            };
+            var resposne = await httpClient.GetAsync(url);
+            return JsonSerializer.Deserialize<IPInfo>(await resposne.Content.ReadAsStringAsync());
+        }
+    }
+}
---
+++ Extensions/Extensions.IP/IpClientExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+namespace Extensions.IP
+{
+    public static class IpClientExtension
+    {
+        public static async Task<IPInfo> GetIpInfoAsync(this string ipAddress)
+        {
+            string url = null;
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                url = "json?fields=66846719";
+            }
+            else
+            {
+                url = $"json/{ipAddress}?fields=66846719";
+            }
+            HttpClient httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://ip-api.com"),
+                Timeout = TimeSpan.FromSeconds(20)
+            };
+            var resposne = await httpClient.GetAsync(url);
+            return JsonSerializer.Deserialize<IPInfo>(await resposne.Content.ReadAsStringAsync());
+        }
+    }
+}
